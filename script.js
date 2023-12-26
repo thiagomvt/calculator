5470afc8d66c5faf45339db94d5869c27882cc4e
Thiago <143139077+thiagomvt@users.noreply.github.com>
2023-12-26 01:53:38 -0300

    Displays the clicked numbers

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
+// Operations functions
 function add(a, b) {return a + b};
 function subtract(a, b) {return a - b};
 function multiply(a, b) {return a * b};
 function divide(a, b) {return a/b};
 
+// Variables to work on functions
 let a = undefined;
 let b = undefined;
 let operator = undefined;
 
-function operate(){
-  operator(a, b);
+// Updates text in display
+function displayText(text){
+let display = document.querySelector('#display');
+display.textContent = (text);
 }
 
-let numbers = Array.from(document.querySelectorAll('.number'));
-console.log(numbers);
+// Get list of number buttons
+let numbers = document.querySelectorAll('.number');
+numbers.forEach(function(element){
+  element.addEventListener('click', handleClick);
+});
 
-// for (number in numbers){
-//   addEventListener(click, function(){
+// Handles number buttons clicks
 
-//   })
-// }
+let text = '';
 
+function handleClick(event){
 
-// let one = document.querySelector('#one');
-// let two = document.querySelector('#two');
-// let three = document.querySelector('#three');
-// let four = document.querySelector('#four');
-// let five = document.querySelector('#five');
-// let six = document.querySelector('#six');
-// let seven = document.querySelector('#seven');
-// let eight = document.querySelector('#eight');
-// let nine = document.querySelector('#nine');
-// let zero = document.querySelector('#zero');
+  let number = event.target.dataset.number;
+
+  switch (number){
+    case '1':
+      text += '1';
+      break;
+    case '2':
+      text += '2';
+      break;
+    case '3':
+      text += '3';
+      break;
+    case '4':
+      text += '4';
+      break;
+    case '5':
+      text += '5';
+      break;
+    case '6':
+      text += '6';
+      break;
+    case '7':
+      text += '7';
+      break;
+    case '8':
+      text += '8';
+      break;
+    case '0':
+      text += '0';
+      break;
+    case '.':
+      text += '.';
+      break;
+  }
+  displayText(text);
+}
